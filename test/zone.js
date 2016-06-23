a81042925ac4fe19850c071c3aa56ed4659701cb
david hdogetts <david.demainlalune@gmail.com>
2016-06-23 16:47:37 +0200

    fix zone data and update zones test file

--- test/zone.js
+++ test/zone.js
@@ -9,18 +9,16 @@ var should = require('should');
 var test = exports;
 
 
-
-
-
-
 describe("zone, assert coherence of data set 'zones.json'", function(){
-
-// a simple test file to assert coherence of data set 'zones.json'
 
   var fs = require('fs'),
       _ = require('lodash'),
       turf = require('turf');
 
+
+  before(function(done){
+    done();
+  });
 
   var zones = JSON.parse(fs.readFileSync('data/zones.json', 'utf-8')),
       zone_1 = _.find(zones.features, {zone_id : '1'}),
@@ -29,52 +27,41 @@ describe("zone, assert coherence of data set 'zones.json'", function(){
       zone_4 = _.find(zones.features, {zone_id : '4'});
 
 
-  before(function(done){
-    done()
+  var pt_on_bourbon_wharf = turf.point([2.355741262435913, 48.85275733753064]),
+      pt_on_orleans_wharf = turf.point([2.3545074462890625, 48.85152896334864]),
+      pt_on_bethune_wharf = turf.point([2.357436418533325, 48.85059177829556]),
+      pt_on_anjou_wharf = turf.point([2.3585522174835205, 48.85189606683644]);
+
+  it("pt_on_bourbon_wharf should be in zone 1 (quai Bourbon)", function(done){
+    turf.inside(pt_on_bourbon_wharf, zone_1).should.be.true();
+    done();
   });
 
-  //
-  // [long,lat]!! long, lat order when defining points in GeoJson
-
-  it("pt1 should be in zone 1", function(done){
-    var pt1 = turf.point([2.357530296, 48.850952711]);
-    turf.inside(pt1, zone_1).should.be.true();
-    done()
-  });
-
-  it("pt1 should NOT be in zone 2", function(done){
-    var pt1 = turf.point([2.357530296, 48.850952711]);
-    turf.inside(pt1, zone_2).should.not.be.true();
-    done()
+  it("pt_on_bourbon_wharf should NOT be in zone 2 (quai Orléans) ", function(done){
+    turf.inside(pt_on_bourbon_wharf, zone_2).should.not.be.true();
+    done();
   });
 
 
-  it("pt2 should be in zone 2", function(done){
-    // pt2 should be in zone 2
-    var pt2 = turf.point([2.355159223, 48.851736342]);
-    turf.inside(pt2, zone_2).should.be.true();
-    done()
+  it("pt_on_orlean_wharf should be in zone 2 (quai Orléans)", function(done){
+    turf.inside(pt_on_orleans_wharf, zone_2).should.be.true();
+    done();
   });
 
-  it("pt3 should be in zone 3", function(done){
-    // pt3 should be in zone 3
-    var pt3 = turf.point([2.355845869, 48.852421126]);
-    turf.inside(pt3, zone_3).should.be.true();
-    done()
+  it("pt_on_bethune_wharf should be in zone 3 (quai Béthune)", function(done){
+    turf.inside(pt_on_bethune_wharf, zone_3).should.be.true();
+    done();
   });
 
-  it("pt4 should be in zone 4", function(done){
-    // pt4 should be in zone 4
-    var pt4 = turf.point([2.358109653, 48.851637506]);
-    turf.inside(pt4, zone_4).should.be.true();
-    done()
+  it("pt_on_anjou_wharf should be in zone 4 (quai Anjou)", function(done){
+    turf.inside(pt_on_anjou_wharf, zone_4).should.be.true();
+    done();
   });
 
+  // is this useful, it seems this only tests turf.js not our data?
   it("generate random points inside zone 4", function(done) {
-    var pt4a= turf.pointOnSurface(zone_4);
-    turf.inside(pt4a, zone_4).should.be.true();
-    done()
-  })
-
-
+    var random_pt_on_zone_4 = turf.pointOnSurface(zone_4);
+    turf.inside(random_pt_on_zone_4, zone_4).should.be.true();
+    done();
+  });
 });
