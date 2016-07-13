9d2e278a76a922bb12527631ead6dfbcd86de895
david hdogetts <david.demainlalune@gmail.com>
2016-07-13 10:52:17 +0200

    gulp prod/dev library build tasks

--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,8 @@ var gulp = require('gulp'),
 
 
 var airtable_json_file_path = './data/airtable_db.json',
-    airtable_zone_ids_converted_json_file_path = './data/airtable_zone_ids_converted.json',
-    env = process.env.NODE_ENV || 'development';
+    airtable_zone_ids_converted_json_file_path = './data/airtable_zone_ids_converted.json';
 
-console.log(util.format("running in %s mode", env));
 
 gulp.task('import_airtable', function(cb){
 
@@ -36,12 +34,56 @@ gulp.task('convert_json_to_lib', ['convert_zone_ids_to_human_readable'], functio
     
     json_to_lib(airtable_zone_ids_converted_json_file_path, './lib/model_data.js');
 
-    var geojson_filpath = (env == 'development') ? './data/zones_dev.json' : './data/zones.json';
+    var env = process.env.NODE_ENV || 'development',
+        geojson_filepath = (env == 'development') ? './data/zones_dev.json' : './data/zones.json';
 
-    json_to_lib(geojson_filpath, './lib/zone_geo_data.js');
+    json_to_lib(geojson_filepath, './lib/zone_geo_data.js');
 
     cb();
 });
 
 
+gulp.task('set_environment_to_production', function(cb){
+
+    process.env.NODE_ENV = 'production';
+    cb();
+});
+
+gulp.task('set_environment_to_development', function(cb){
+
+    process.env.NODE_ENV = 'development';
+    cb();
+});
+
+gulp.task('build_distribution', ['convert_json_to_lib'], function(cb){
+
+    var spawn = require('child_process').spawn,
+        webpack = spawn('webpack');
+
+    webpack.stderr.on('data', function(data) {
+        console.log('stderr:', data);
+    });
+
+    webpack.on('close', function(code) {
+        if(code != 0){
+            cb(new error("webpack process failure"));
+        }else{
+            cb();
+        }
+    });
+
+});
+
 gulp.task('build_data_model', ['import_airtable', 'convert_zone_ids_to_human_readable', 'convert_json_to_lib']);
+
+gulp.task('build_library_for_production', ['set_environment_to_production',
+                                           'import_airtable',
+                                           'convert_zone_ids_to_human_readable',
+                                           'convert_json_to_lib',
+                                           'build_distribution']);
+
+gulp.task('build_library_for_development', ['set_environment_to_development',
+                                           'import_airtable',
+                                           'convert_zone_ids_to_human_readable',
+                                           'convert_json_to_lib',
+                                           'build_distribution']);
